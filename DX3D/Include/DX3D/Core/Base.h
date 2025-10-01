54cf3ef74224a47bea441650beaf6787f4c77fe3
Reinier The Ghost <reinierrolbrouwer@gmail.com>
2025-10-01 13:55:53 +0200

    Add project files.

---
+++ DX3D/Include/DX3D/Core/Base.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace dx3d
+{
+
+	class Base
+	{
+	public:
+		Base();
+		virtual ~Base();
+	};
+}
+
+
+
---
+++ ZooTycoon/Main.cpp
@@ -0,0 +1,33 @@
+#include <DX3D/All.h>
+
+int main() 
+{
+
+	try
+	{
+		dx3d::Game game{};
+		game.run();
+	}
+	catch (const std::runtime_error&)
+	{
+		return EXIT_FAILURE;
+	}
+	catch (const std::invalid_argument&)
+	{
+		return EXIT_FAILURE;
+	}
+	catch (const std::exception&)
+	{
+		return EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		return EXIT_FAILURE;
+	}
+
+
+
+	return EXIT_SUCCESS;
+
+	return 0;
+}
